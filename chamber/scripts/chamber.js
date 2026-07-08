2f8bd5f47a95d6565f3856e3750cd26169204156
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-08 13:10:35 -0500

    Gathered images for the web page. Modified the pacge to include mobile first design. Added emojies for dark mode and footer. (Need to replace with better icons)

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -5,18 +5,36 @@ const displayBusinesses = (businesses) => {
   businesses.forEach((business) => {
     let card = document.createElement('section');
     let busname = document.createElement('h2');
+    let tagline = document.createElement('p');
     let logo = document.createElement('img');
+    let details = document.createElement('div');
+    let email = document.createElement('p');
+    let phone = document.createElement('p');
+    let url = document.createElement('p');
 
     busname.textContent = `${business.businessname}`;
+    tagline.textContent = business.businesstagline;
+    tagline.className = 'tagline';
 
     logo.setAttribute('src', business.imageurl);
-    logo.setAttribute('alt', `Portrait of ${business.businessname}`);
+    logo.setAttribute('alt', `Logo of ${business.businessname}`);
     logo.setAttribute('loading', 'lazy');
-    logo.setAttribute('width', '340');
-    logo.setAttribute('height', '440');
+    logo.setAttribute('width', '150');
+    logo.setAttribute('height', '150');
+
+    details.className = 'details';
+    email.innerHTML = `<strong>EMAIL:</strong> ${business.email}`;
+    phone.innerHTML = `<strong>PHONE:</strong> ${business.phone}`;
+    url.innerHTML = `<strong>ADDRESS:</strong> ${business.address}`;
+
+    details.appendChild(email);
+    details.appendChild(phone);
+    details.appendChild(url);
 
     card.appendChild(busname);
+    card.appendChild(tagline);
     card.appendChild(logo);
+    card.appendChild(details);
 
     cards.appendChild(card);
   });
