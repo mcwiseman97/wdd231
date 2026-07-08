deeb5027aa0469ab3d076d8d8c89d8f2420f65dd
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-08 12:00:42 -0500

    Update directory.html and business.json; add business data and improve metadata

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,39 @@
+const url = 'https://mcwiseman97.github.io/wdd231/chamber/chamber/scripts/chamber.js';
+
+const cards = document.querySelector('#cards');
+
+const displayBusinesses = (businesses) => {
+  businesses.forEach((business) => {
+    let card = document.createElement('section');
+    let busname = document.createElement('h2');
+    let logo = document.createElement('img');
+
+    busname.textContent = `${business.businessname}`;
+
+    logo.setAttribute('src', business.imageurl);
+    logo.setAttribute('alt', `Portrait of ${business.businessname}`);
+    logo.setAttribute('loading', 'lazy');
+    logo.setAttribute('width', '340');
+    logo.setAttribute('height', '440');
+
+    card.appendChild(busname);
+    card.appendChild(logo);
+
+    cards.appendChild(card);
+  });
+};
+
+async function getBusinessData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error status: ${response.status}`);
+    }
+    const data = await response.json();
+    displayBusinesses(data.businesses);
+  } catch (error) {
+    console.error('Failed to fetch business data:', error);
+  }
+}
+
+getBusinessData(url);
