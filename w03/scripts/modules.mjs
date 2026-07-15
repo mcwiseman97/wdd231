418bd1a719e120eab1420234eab1bd5d34f27636
Michael Wiseman <mcwiseman97@gmail.com>
2026-07-14 20:52:37 -0500

    Refactor module structure by moving setTitle and renderSections functions to output.mjs and updating event listeners in modules.mjs to utilize these functions.

--- w03/scripts/modules.mjs
+++ w03/scripts/modules.mjs
@@ -1,29 +1,17 @@
 import byuiCourse from "./course.mjs";
 import { setSectionSelection } from "./sections.mjs";
-
-
-function setTitle(course) {
-  document.querySelector("#courseName").textContent = course.name;
-  document.querySelector("#courseCode").textContent = course.code;
-}
-
-function renderSections(sections) {
-  const html = sections.map(
-    (section) => `<tr>
-    <td>${section.sectionNumber}</td>
-    <td>${section.enrolled}</td>
-    <td>${section.instructor}</td></tr>`
-  );
-  document.querySelector("#sections").innerHTML = html.join("");
-}
+import { setTitle, renderSections } from "./output.mjs";
 
 document.querySelector("#enrollStudent").addEventListener("click", function () {
   const sectionNum = Number(document.querySelector("#sectionNumber").value);
   byuiCourse.changeEnrollment(sectionNum);
+  renderSections(byuiCourse.sections);
 });
+
 document.querySelector("#dropStudent").addEventListener("click", function () {
   const sectionNum = Number(document.querySelector("#sectionNumber").value);
   byuiCourse.changeEnrollment(sectionNum, false);
+  renderSections(byuiCourse.sections);
 });
 
 setTitle(byuiCourse);
